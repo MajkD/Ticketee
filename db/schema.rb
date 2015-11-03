38bb513858c0022ff5ba0b2ebc893b24963f51cd
Mikael Danielsson <majkdan@gmail.com>
2015-11-03 16:49:09 +0100

    Link ticket and users upon ticket creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102155742) do
+ActiveRecord::Schema.define(version: 20151103130710) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 20151102155742) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "author_id"
   end
 
+  add_index "tickets", ["author_id"], name: "index_tickets_on_author_id", using: :btree
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id", using: :btree
 
   create_table "users", force: true do |t|
@@ -47,5 +49,8 @@ ActiveRecord::Schema.define(version: 20151102155742) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  Foreigner.load
+  add_foreign_key "tickets", "users", name: "tickets_author_id_fk", column: "author_id"
 
 end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Users can create new tickets" do
+    let(:user) { FactoryGirl.create(:user) }
+
   before do
+    login_as(user)
     project = FactoryGirl.create(:project, name: "Interner Explorer")
 
     visit project_path(project)
     click_link "New Ticket"
   end
 
-  scenario "With valid attributes" do
+  scenario "with valid attributes" do
     fill_in "Name", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
     click_button "Create Ticket"
     expect(page).to have_content "Ticket has been created"
+    within("#ticket") do
+      expect(page).to have_content "Author: #{user.email}"
+    end
   end
 
   scenario "when providing invalid attributes" do
