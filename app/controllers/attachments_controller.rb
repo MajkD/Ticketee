60f71b35600a7f98aa3f624ecd2757d96a5cc73a
Mikael Danielsson <majkdan@gmail.com>
2015-12-04 09:07:08 +0100

    page no longer crashes when uploaded file error

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -8,6 +8,7 @@ class AttachmentsController < ApplicationController
   end
 
   def new
+    require 'pry'; binding.pry
     @ticket = Ticket.new
     @ticket.attachments.build
     @index = params[:index].to_i
